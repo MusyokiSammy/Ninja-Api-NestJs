a0dc879025b5b0dcd776a60c79bf4b9d8c8b8916
MusyokiSammy <sammymusyoki328@gmail.com>
2023-10-18 10:14:37 +0300

    controllers-update

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,10 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
-
-// GET /ninjas --> []
-// GET /ninjas/:id --> {id: string}
-// POST /ninjas/
-// PUT /ninjas/:id --> {id: string}
-// DELETE /ninjas/:id --> {id: string}
+export class NinjasController {
+  // GET /ninjas --> []
+  @Get()
+  getNinjas() {
+    return [];
+  }
+  // GET /ninjas/:id --> {id: string}
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+  // POST /ninjas/
+  @Post()
+  createNinja() {
+    return {};
+  }
+  // PUT /ninjas/:id --> {id: string}
+  @Put()
+  updateNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+  // DELETE /ninjas/:id
+  @Delete()
+  deleteNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+}
